dbfbef5dd7eead9dd16ec69a75614d5473ededcb
Daniel Gasienica <daniel@gasienica.ch>
2011-04-03 03:50:40 -0700

    Add tests.

--- test/index.js
+++ test/index.js
@@ -1,27 +1,44 @@
-var fs = require('fs');
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Facebook Node.js SDK
+//
+//  Copyright 2011 Daniel Gasienica <daniel@gasienica.ch>
+//  Copyright 2010 Facebook
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain
+//  a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and limitations
+//  under the License.
+//
+//////////////////////////////////////////////////////////////////////////////
 var assert = require('assert');
 var facebook = require('../lib/facebook');
+var fs = require('fs');
 
-var SIGNED_REQUEST = 'vlXgu64BQGFSQrY0ZcJBZASMvYvTHu9GQ0YM9rjPSso.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsIjAiOiJwYXlsb2FkIn0';
-// var SIGNED_REQUEST = 'WGvK-mUKB_Utg0l8gSPvf6smzacp46977pTtcRx0puE.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEyOTI4MjEyMDAsImlzc3VlZF9hdCI6MTI5MjgxNDgyMCwib2F1dGhfdG9rZW4iOiIxNTI1NDk2ODQ3NzczMDJ8Mi5ZV2NxV2k2T0k0U0h4Y2JwTWJRaDdBX18uMzYwMC4xMjkyODIxMjAwLTcyMTU5OTQ3NnxQaDRmb2t6S1IyamozQWlxVldqNXp2cTBmeFEiLCJ1c2VyIjp7ImxvY2FsZSI6ImVuX0dCIiwiY291bnRyeSI6ImF1In0sInVzZXJfaWQiOiI3MjE1OTk0NzYifQ';
-var APP_SECRET = 'secret';
-var EXPECTED_DATA = {
-    'algorithm': 'HMAC-SHA256',
-    '0': 'payload'
-};
-
-facebook.decodeSignedRequest(SIGNED_REQUEST, APP_SECRET);
-// assert.deepEqual(facebook.decodeSignedRequest(SIGNED_REQUEST, APP_SECRET),
-                 // EXPECTED_DATA);
-
-var tests = fs.readFileSync(__dirname + '/data.txt').toString().split('\n');
-
+// Test: decodeSignedRequest
+var tests = loadTests('signed-requests-data.json');
 tests.forEach(function (test) {
-    if (!test || test[0] == '#') { return; }
-    var line = test.split(' ');
-    name = line[0];
-    input = line[1];
-    output = line[2];
-    var data = facebook.decodeBase64URL(input);
-    assert.strictEqual(data, output);
+    var actual = facebook.decodeSignedRequest(test.signedRequest, test.appSecret);
+    var expected = test.expected;
+    assert.deepEqual(actual, expected);
 });
+
+// Test: Base64 URL decode
+var tests = loadTests('base64-url-decode-data.json');
+tests.forEach(function (test) {
+    var actual = facebook.base64URLDecode(test.input);
+    var expected = test.output;
+    assert[test['test']](actual, expected, test.name);
+});
+
+// Helpers
+function loadTests(filename) {
+    return JSON.parse(fs.readFileSync(__dirname + '/' + filename).toString());
+}
